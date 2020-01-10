453e3b14400d2807442eebbe750a85862464f735
Cyrus Roshan <cyrusroshan@users.noreply.github.com>
2020-01-10 13:35:30 -0800

    Pass builds.

--- pkg/controller/service/service.go
+++ pkg/controller/service/service.go
@@ -2906,7 +2906,11 @@ func (s *Service) setDeviceServiceStatus(w http.ResponseWriter, r *http.Request,
 	}
 
 	if err := s.deviceServiceStatuses.SetDeviceServiceStatus(r.Context(), project.ID, device.ID,
-		applicationID, service, setDeviceServiceStatusRequest.CurrentReleaseID, setDeviceServiceStatusRequest.ContainerStatus,
+		applicationID,
+		service,
+		setDeviceServiceStatusRequest.CurrentReleaseID,
+		setDeviceServiceStatusRequest.ContainerStatus,
+		setDeviceServiceStatusRequest.ContainerError,
 	); err != nil {
 		log.WithError(err).Error("set device service status")
 		w.WriteHeader(http.StatusInternalServerError)
